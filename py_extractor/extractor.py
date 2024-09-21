b1bf78ff3d07ac3adc0e91e44fefc2bbbf08b5a4
Tammy Young <tammy.q.young@gmail.com>
2024-09-20 22:30:01 -0600

    feat: implement transcript to speaker notes

--- py_extractor/extractor.py
+++ py_extractor/extractor.py
@@ -1,6 +1,8 @@
 import os, zipfile, shutil
 from pydub import AudioSegment
 import speech_recognition as sr
+from pptx import Presentation
+from pathlib import Path
 
 
 audio_extensions = ('.aiff', '.au', '.mid', '.midi', '.mp3', '.m4a', '.mp4', '.wav', '.wma')
@@ -18,13 +20,8 @@ def prepare_voice_file(path):
     audio_file.export(wav_file, format='wav')
     return wav_file
 
-def main():
-    print('Extracting...')
-
-    if os.path.isdir(ppt_dir):
-        shutil.rmtree(ppt_dir)
-    
-    for power in init_files :
+def extract_audio_files(directory):
+    for power in directory:
 
         fbase = "".join(power.split(".")[:-1]).replace(' ', '_')
 
@@ -53,32 +50,56 @@ def main():
         os.remove(new_zip)
         break
 
+    return power
+
+def transcribe(audio_file_paths, result_subdir, pptx_file_name):
+    r = sr.Recognizer()
+    total_num = len(audio_file_paths)
+    processing_count = 1
+    pptx_file_path = f"{result_subdir}/{pptx_file_name}"
+    for f in audio_file_paths:
+        f_name = f.split("/")[-1].split(".")[0]
+        slide_num = int(f_name.split('media')[1])
+        print(f"Processing {processing_count}/{total_num}...")
+        wav_file = prepare_voice_file(f)
+        with sr.AudioFile(wav_file) as source:
+            audio_data = r.record(source)
+            text = r.recognize_google(audio_data)
+            with open(f"{result_subdir}/{output_file_prefix}{f_name}.txt", 'w') as out_file:
+                to_notes(text, pptx_file_path, slide_num - 1)
+        processing_count += 1
+
+def to_notes(note, pptx_fname, slide_num):
+    pptx_file = Path(pptx_fname)
+    prs = Presentation(pptx_file)
+    slide = prs.slides[slide_num]
+    slide.notes_slide.notes_text_frame.text = note
+    prs.save(pptx_file)
+
+def main():
+    print('Extracting...')
+
+    if os.path.isdir(ppt_dir):
+        shutil.rmtree(ppt_dir)
+    
+    file = extract_audio_files(init_files)
+
     audio_files = os.listdir(media_dir)
     audio_file_paths = [f"{media_dir}/{f}" for f in audio_files]
+    sorted_audio_files = sorted(audio_file_paths, key=lambda x: int(x.split('/')[-1].split('.')[0].split('media')[1]))
 
-    if audio_file_paths:
-        r = sr.Recognizer()
-        total_num = len(audio_file_paths)
-        processing_count = 1
+    if not sorted_audio_files:
+        return
 
-        result_dir_relative = "".join(power.split(".")[:-1]).replace(" ", "_")
-        result_subdir = f"{results_dir}/{result_dir_relative}"
-        try:
-            os.mkdir(result_subdir)
-        except: pass
+    result_dir_relative = "".join(file.split(".")[:-1]).replace(" ", "_")
+    result_subdir = f"{results_dir}/{result_dir_relative}"
 
-        shutil.copy(power, result_subdir)
+    try:
+        os.mkdir(result_subdir)
+    except: pass
 
-        for f in audio_file_paths:
-            f_name = f.split("/")[-1].split(".")[0]
-            print(f"Processing {processing_count}/{total_num}...")
-            wav_file = prepare_voice_file(f)
-            with sr.AudioFile(wav_file) as source:
-                audio_data = r.record(source)
-                text = r.recognize_google(audio_data)
-                with open(f"{result_subdir}/{output_file_prefix}{f_name}.txt", 'w') as out_file:
-                    out_file.write(text)
-            processing_count += 1
+    shutil.copy(file, result_subdir)
+    transcribe(sorted_audio_files, result_subdir, file)
     
     shutil.rmtree(media_dir)
 
